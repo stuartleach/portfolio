401667db6c3e76b508971740172d03270d27b64b
Stu Leach <jstuartleach@gmail.com>
2021-09-11 14:41:04 -0400

    make cards and debug ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { JsxElement } from 'typescript'
+import {
+	Box,
+	Button,
+	ThemeProvider,
+	Circle,
+	Link,
+	Center,
+	Text,
+	ChakraProvider,
+	Image,
+} from '@chakra-ui/react'
+import Header from './Header'
+import faceThing from './wFA9wU01.svg'
+import backgroundThing from './Wave-10s-1323px.svg'
+import Footer from './Footer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Works from './Works'
+
+function App(props: object): JSX.Element {
+	return (
+		<Box color='white' fontFamily='roboto'>
+			<Box
+				className='App'
+				backgroundImage={backgroundThing}
+				backgroundPosition='top'
+				backgroundSize='cover'
+				backgroundRepeat='no-repeat'
+				height='200vh'
+				zIndex='-1'
+			>
+				<Image
+					height='200px'
+					borderRadius='100%'
+					position='fixed'
+					zIndex='-2'
+					src={faceThing}
+				></Image>
+				<Center>
+					<Header />
+				</Center>
+				<Works />
+				<Box
+					position='absolute'
+					bottom='0px'
+					width='100%'
+					p='0'
+					display='flex'
+					maxHeight='50%'
+				></Box>
+			</Box>
+			<Box position='relative' marginBottom='40px'>
+				<Footer />
+			</Box>
+		</Box>
+	)
 }
 
-export default App;
+export default App

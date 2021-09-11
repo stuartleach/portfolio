894e13204ff270c1e0d43dba8783f0565f84014b
Stu Leach <jstuartleach@gmail.com>
2021-09-11 17:27:04 -0400

    correct scroll

--- src/App.tsx
+++ src/App.tsx
@@ -21,61 +21,55 @@ import Works from './Works'
 
 function App(props: object): JSX.Element {
 	return (
-		<Parallax pages={2} style={{ top: '0', left: '0' }}>
-			<Box color='white' fontFamily='roboto'>
-				<Box
-					className='App'
-					backgroundImage={backgroundThing}
-					backgroundPosition='top'
-					backgroundSize='cover'
-					bgColor='black'
-					// backgroundRepeat='no-repeat'
-					height='200vh'
-					zIndex='-1'
-				>
-					<ParallaxLayer
-						offset={0}
-						speed={-2.5}
-						style={{
-							display: 'flex',
-							justifyContent: 'center',
-							alignItems: 'center',
-						}}
+		<Box>
+			<Parallax pages={2} style={{ top: '0', left: '0' }}>
+				<Box color='white' fontFamily='roboto'>
+					<Box
+						className='App'
+						backgroundImage={backgroundThing}
+						backgroundPosition='bottom'
+						backgroundSize='cover'
+						bgColor='black'
+						// backgroundRepeat='no-repeat'
+						height='300vh'
+						zIndex='-1'
 					>
-						<Center>
-							<Header />
-						</Center>
-					</ParallaxLayer>
+						<ParallaxLayer
+							offset={0}
+							speed={-2.5}
+							style={{
+								display: 'flex',
+								justifyContent: 'center',
+								alignItems: 'center',
+								// height: '100%',
+							}}
+						>
+							<Center>
+								<Header />
+							</Center>
+						</ParallaxLayer>
 
-					<ParallaxLayer
-						offset={1}
-						speed={1.5}
-						style={{
-							display: 'block',
-							justifyContent: 'center',
-							alignItems: 'center',
-							color: 'white',
-						}}
-					>
-						<Works />
+						<ParallaxLayer
+							offset={1}
+							speed={1.5}
+							style={{
+								// display: 'block',
+								justifyContent: 'center',
+								alignItems: 'center',
+								color: 'white',
+								overflow: 'scroll',
+							}}
+						>
+							<Works />
 
-						<Box position='relative' marginTop='10vh'>
-							<Footer />
-						</Box>
-					</ParallaxLayer>
-					<ParallaxLayer
-						offset={2}
-						speed={1.5}
-						style={{
-							display: 'inline-block',
-							justifyContent: 'center',
-							alignItems: 'center',
-							color: 'white',
-						}}
-					></ParallaxLayer>
+							<Box position='relative' marginTop='10vh' mb='20px'>
+								<Footer />
+							</Box>
+						</ParallaxLayer>
+					</Box>
 				</Box>
-			</Box>
-		</Parallax>
+			</Parallax>
+		</Box>
 	)
 }
 
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,31 +1,28 @@
-import { Text, Box } from '@chakra-ui/layout'
+import { Text, Box, Container } from '@chakra-ui/layout'
 
 const Header = () => {
 	return (
 		<Box>
-			<Text
-				fontWeight='600'
-				fontSize='8vw'
-				letterSpacing='-0.222vw'
-				mt='20vh'
-			>
-				Hi, I'm Stuart Leach
-			</Text>
-			<Text align='left' fontSize='3vw'>
-				<code>
-					<Text
-						fontSize='20px'
-						mt='-100px'
-						textAlign='right'
-						position='static'
-					>
-						full-stack react developer
-						<br />
-						<br />
-						pancake scientist
+			<Container fontWeight='600' fontSize='8vw' mt='20vh'>
+				<Box letterSpacing='-0.222vw'>Hi, I'm Stuart Leach</Box>
+				<Box>
+					<Text align='left' fontSize='3vw'>
+						<code>
+							<Text
+								fontSize='20px'
+								// mt='100px'
+								textAlign='right'
+								position='static'
+							>
+								full-stack react developer
+								<br />
+								<br />
+								pancake scientist
+							</Text>
+						</code>
 					</Text>
-				</code>
-			</Text>
+				</Box>
+			</Container>
 		</Box>
 	)
 }

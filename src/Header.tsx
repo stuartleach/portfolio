cd6c14df790d6365d3d33feb0f2f24d20a5e1808
Stu Leach <jstuartleach@gmail.com>
2021-09-11 19:05:11 -0400

    fontsize

--- src/Header.tsx
+++ src/Header.tsx
@@ -4,12 +4,12 @@ const Header = () => {
 	return (
 		<Box>
 			<Container fontWeight='600' mt='20vh' textAlign='right' p='80px'>
-				<Box fontSize='7em'>Hi, I'm Stuart Leach</Box>
+				<Box fontSize='4em'>Hi, I'm Stuart Leach</Box>
 				<Box>
 					<Text align='right' fontSize=''>
 						<code>
 							<Text
-								fontSize='2em'
+								fontSize='em'
 								// mt='100px'
 								textAlign='right'
 								position='static'
